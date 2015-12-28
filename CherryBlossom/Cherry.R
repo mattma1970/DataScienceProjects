6d221e7ccac30726842ca266c4b8ae2a870ba65b
mattma1970 <mattma1970@gmail.com>
2015-12-28 17:22:14 +1100

    Retructure folders

--- CherryBlossom/Cherry.R
+++ CherryBlossom/Cherry.R
@@ -28,20 +28,90 @@ scrapeData =
     txt=xmlValue(preNode[[1]])
   }
   if (txt!=''){
-    lines = strsplit(txt,'\\r\\n')[[1]][-(1:intHeaderLines)]
+    if (intHeaderLines>0){
+        lines = strsplit(txt,'\\r\\n')[[1]][-(1:intHeaderLines)]
+    } else {lines = strsplit(txt,'\\r\\n')[[1]] }
   }
   else {
-    lines = as.vector(lines)
-    lines = lines[-(1:intHeaderLines)]
+    lines = unlist(lines)
+    if (intHeaderLines>0)
+    {
+      lines = lines[-(1:intHeaderLines)]
+    }
   }
   
   print(t(c("year",year,"  #rows=",length(lines))))
-  return (lines)
+  return (removePreamble(lines))
+  }
+
+
+removePreamble = 
+  # Remove the preamble and set the header row ot lower case
+  #@results is a character vector.
+  # returns processed list.
+  function(results){
+
+    dividerRowNumber = grep("^={4}",results)
+    if (dividerRowNumber>2){
+        results = results[-(1:(dividerRowNumber-2))]
+    }
+    results[1]=tolower(results[1])
+    return(results)
 }
+
+
+getColumnLocs = 
+  #get the character positions for the columns.
+  # returns the bounding locations of the columns in character location units.
+  function(spacerRow){
+    locs = gregexpr("\\s",spacerRow)[[1]]
+    if (substring(spacerRow,nchar(spacerRow),nchar(spacerRow))!=" "){
+      return(c(0,locs,nchar(spacerRow)+1))
+    } else {
+      return(c(0,locs))
+    }
+  }
+
+
+
+getValByCol =
+  # get the values from a column specified by the name 
+  # @colNames is the names of the columns to retrieve vals for
+  # @headerRow is the columnnames row in the data
+  # @searchLocations are the bounding character positions delimiting the column
+  # @returns a matrix with each column corresponding to a column name, row one is start char loc and row 2 is end char location of column
+  function(colNames,headerRow,searchLocations){
+    sapply(colNames,
+           function(name, headerRow,searchLocations){
+             startPos = regexpr(name,headerRow)[[1]]  #returns base 1 location of the column name.
+             if(startPos==-1)
+               return (c(NA,NA))
+             index = sum(startPos>=searchLocations)  # find the index in the searchLocations
+             c(searchLocations[index]+1,searchLocations[index+1]-1)
+           },
+           headerRow = headerRow, searchLocations=searchLocations)  #note using sapply not mapply as the paras headerrow and searchlocations are not in applied sequentially to the parameter of the function
+  }
+
+
+extractVariables = 
+  # take a vector char and split into a matrix of seperate variables. Data type remains char.
+  function(myData,varNames = c("name","home","ag","gun","net","time")){
+    # data will always have first row as the columns names and second row as the spacer row from which the column delimiter places will be foudn
+    colDelimiters = getColumnLocs(myData[2])   #char locations of col boundaries
+    liColBoundaries = getValByCol(varNames,myData[1],colDelimiters)   # the boundaries corresponding to the col names supplied
+    
+    Values = mapply(substr,list(myData[-(1:2)]),start=liColBoundaries[1,],stop=liColBoundaries[2,])
+    colnames(Values)=varNames
+    return(Values)
+  }
+
+
+
 
 getAllMensResults = 
   # Get all the mens results for the cherryblossom race 1999-2012.
-  function(){
+  # Returns list of results, one element per year.
+  function(intHeaderRows){
 ubase = "http://www.cherryblossom.org/"
 #### Handling multiple pages of results. 
 ##urls = paste(ubase,"results/",1999:2012,"/",1999:2012,"cucb10m-m.htm",sep="")
@@ -56,12 +126,23 @@ menURLs = c("cb99m.htm","cb003m.htm","results/2001/oof_m.html","results/2002/oof
 
 urls = paste(ubase,menURLs,sep="")
 years=1999:2012
-allMensResults = mapply(scrapeData,url = urls,year=years)
+allMensResults = mapply(scrapeData,url = urls,year=years, intHeaderLines=rep.int(intHeaderRows,length(years)))
 names(allMensResults)=1999:2012
 return(allMensResults)
   }
 
+
 ### Main   ########################
 
-#get the data by scraping the web site.
-mensResults = getAllMensResults()
+#get the data by scraping the web site. 
+mensResults = getAllMensResults(intHeaderRows=0)
+
+# get cols of interest only and return a char matrix
+mensResultsMatrix = lapply(mensResults, extractVariables) 
+
+# parse values to appropriate type.
+
+
+
+
+
